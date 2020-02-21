a33daf6dd49a1fb464ebcf47a5f13d3b51550431
Henry <mli42@bwh.harvard.edu>
2020-02-21 13:44:41 -0500

    add login

--- app.js
+++ app.js
@@ -9,4 +9,25 @@ app.get('/api', (req, res) => {
     });
 });
 
+app.post('/api/post', (req, res) => {
+    res.json({
+        message: 'Post Created'
+    })
+});
+
+app.post('/api/login', (req, res) => {
+    // Create User
+    const user = {
+        id: 1,
+        username: 'brad',
+        email: 'gmail'
+    }
+    jwt.sign({user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
+});
+
+
 app.listen(5000, () => console.log('Server running on port 5000'));
