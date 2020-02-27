0de631f4386d5f1d76c95fd8693c7b7b6c287878
Henry <mli42@bwh.harvard.edu>
2020-02-27 12:41:05 -0500

    google strategy setup

--- config/passport.setup.js
+++ config/passport.setup.js
@@ -4,6 +4,8 @@ const GoogleStrategy = require('passport-google-oauth20');
 passport.use(
     new GoogleStrategy({
     // options for google strat
+    clientID: '768185898266-tbo672gddghlvvor9jb1dvfjjsnmors8.apps.googleusercontent.com',
+    clientSecret: 'tsmMd5BpMXZwtF7ap1_v3l1A'
     }), () => {
         // passport callback function
     }
